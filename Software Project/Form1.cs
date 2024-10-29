0228ed7e95bec8a579a11531a2bc2fcb0e78db05
Ruairi Tierney <RT667@student.aru.ac.uk>
2024-10-29 11:30:52 +0000

    GUI file created

--- Software Project/Form1.cs
+++ Software Project/Form1.cs
@@ -16,5 +16,20 @@ namespace Software_Project
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
