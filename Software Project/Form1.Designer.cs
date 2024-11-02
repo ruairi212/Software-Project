37402849318e871fe910543af14d193a7022ef79
Ruairi Tierney <ruairitierney212@gmail.com>
2024-11-02 15:51:30 +0000

    Logo imported no functionality

--- Software Project/Form1.Designer.cs
+++ Software Project/Form1.Designer.cs
@@ -34,13 +34,15 @@
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(210, 24);
+            this.button1.Location = new System.Drawing.Point(193, 24);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(118, 33);
             this.button1.TabIndex = 0;
@@ -51,6 +53,8 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button6);
             this.panel1.Controls.Add(this.button7);
             this.panel1.Controls.Add(this.button1);
@@ -64,7 +68,7 @@
             // button5
             // 
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(334, 24);
+            this.button5.Location = new System.Drawing.Point(317, 24);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(122, 33);
             this.button5.TabIndex = 4;
@@ -74,7 +78,7 @@
             // button6
             // 
             this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(584, 24);
+            this.button6.Location = new System.Drawing.Point(567, 24);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(117, 33);
             this.button6.TabIndex = 5;
@@ -85,7 +89,7 @@
             // button7
             // 
             this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button7.Location = new System.Drawing.Point(462, 24);
+            this.button7.Location = new System.Drawing.Point(445, 24);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(116, 33);
             this.button7.TabIndex = 6;
@@ -97,13 +101,22 @@
             // 
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(183, 118);
+            this.button2.Location = new System.Drawing.Point(687, 7);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(136, 66);
+            this.button2.Size = new System.Drawing.Size(110, 66);
             this.button2.TabIndex = 7;
             this.button2.Text = "Login/signup";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Software_Project.Properties.Resources.Screenshot_2024_11_02_154912;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(160, 79);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
@@ -111,12 +124,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -129,6 +142,7 @@
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
